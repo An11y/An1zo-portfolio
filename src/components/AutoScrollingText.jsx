72e75a1671ea202e4f0f47e64137f22aa2e70c8c
an11y <fedukfeduc@gmail.com>
2025-02-06 16:32:55 +0300

    change bg contact and fix 50/50 autoscroll

--- src/components/AutoScrollingText.jsx
+++ src/components/AutoScrollingText.jsx
@@ -1,24 +1,19 @@
 "use client";
 
 import React from "react";
-import { motion } from "framer-motion";
+
 
 const AutoScrollingText = () => {
     return (
-        <div className="fixed bottom-0 left-0 w-full bg-transparent text-white py-2 md:py-2 overflow-hidden">
-            <motion.div
-                className="flex space-x-[500px] text-lg md:text-sm whitespace-nowrap font-light"
-                animate={{ x: ["0%", "-100%"] }}
-                transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
-            >
-                {[...Array(2)].map((_, i) => (
-                    <div key={i} className="flex space-x-[500px]">
-                        {[...Array(10)].map((_, j) => (
-                            <span key={j}>Designed and Developed by person from forest</span>
-                        ))}
-                    </div>
+        <div className="fixed flex bottom-0 left-0 w-full bg-transparent text-white py-2 overflow-hidden">
+            <div className="flex space-x-[250px] text-sm whitespace-nowrap font-light animate-scroll">
+                {[...Array(10)].map((_, i) => (
+                    <span key={i}>Designed and Developed by person from forest</span>
                 ))}
-            </motion.div>
+                {[...Array(10)].map((_, i) => (
+                    <span key={i}>Designed and Developed by person from forest</span>
+                ))}
+            </div>
         </div>
     );
 };
